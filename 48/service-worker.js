bd0aca9ff45bee9db2484eb40adbe3c7a190daa3
foxjony <37145483+foxjony@users.noreply.github.com>
2024-09-27 19:12:57 +0300

    Delete 48/service-worker.js

--- 48/service-worker.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
-    .then(function(registration) {
-      console.log('Service Worker зареєстровано:', registration);
-    })
-    .catch(function(error) {
-      console.log('Помилка при реєстрації Service Worker:', error);
-    });
-}
